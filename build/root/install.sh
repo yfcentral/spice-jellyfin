1a9e493b3869fdec9c72ff4f19d5b6b0e07a8bbb
yfcentral <70697100+yfcentral@users.noreply.github.com>
2023-10-16 15:17:25 +0100

    Add files via upload

---
+++ build/root/install.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+# exit script if return code != 0
+set -e
+
+# release tag name from buildx arg, stripped of build ver using string manipulation
+RELEASETAG="${1//-[0-9][0-9]/}"
+
+# target arch from buildx arg
+TARGETARCH="${2}"
+
+if [[ -z "${RELEASETAG}" ]]; then
+	echo "[warn] Release tag name from build arg is empty, exiting script..."
+	exit 1
+fi
+
+if [[ -z "${TARGETARCH}" ]]; then
+	echo "[warn] Target architecture name from build arg is empty, exiting script..."
+	exit 1
+fi
+
+# build scripts
+####
+
+# download build scripts from github
+curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /tmp/scripts-master.zip -L https://github.com/binhex/scripts/archive/master.zip
+
+# unzip build scripts
+unzip /tmp/scripts-master.zip -d /tmp
+
+# move shell scripts to /root
+mv /tmp/scripts-master/shell/arch/docker/*.sh /usr/local/bin/
+
+
+# pacman packages
+####
+
+# define pacman packages
+pacman_packages="git libva-intel-driver intel-media-driver intel-media-sdk onevpl-intel-gpu intel-compute-runtime jellyfin-server jellyfin-ffmpeg"
+
+# install compiled packages using pacman
+if [[ ! -z "${pacman_packages}" ]]; then
+	pacman -S --needed $pacman_packages --noconfirm
+fi
+
+# create /var/empty to fix access denied message from dotnet during build of jellyfin
+mkdir -p /var/empty && chmod -R 777 /var/empty
+
+# delme - required to fix up cert issues when compiling .dot
+# custom
+####
+
+package_name="ca-certificates-mozilla.tar.zst"
+
+# download mozilla certs
+rcurl.sh -o "/tmp/${package_name}" "https://github.com/binhex/packages/raw/master/compiled/${TARGETARCH}/${package_name}"
+
+# install mozilla certs
+pacman -U "/tmp/${package_name}" --noconfirm
+# /delme - required to fix up cert issues when compiling .dot
+
+# aur packages
+####
+
+# define aur packages
+aur_packages=""
+
+# call aur install script (arch user repo)
+source aur.sh
+
+# container perms
+####
+
+# define comma separated list of paths
+install_paths="/usr/lib/jellyfin,/var/lib/jellyfin,/home/nobody"
+
+# split comma separated string into list for install paths
+IFS=',' read -ra install_paths_list <<< "${install_paths}"
+
+# process install paths in the list
+for i in "${install_paths_list[@]}"; do
+
+	# confirm path(s) exist, if not then exit
+	if [[ ! -d "${i}" ]]; then
+		echo "[crit] Path '${i}' does not exist, exiting build process..." ; exit 1
+	fi
+
+done
+
+# convert comma separated string of install paths to space separated, required for chmod/chown processing
+install_paths=$(echo "${install_paths}" | tr ',' ' ')
+
+# set permissions for container during build - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
+chmod -R 775 ${install_paths}
+
+# create file with contents of here doc, note EOF is NOT quoted to allow us to expand current variable 'install_paths'
+# we use escaping to prevent variable expansion for PUID and PGID, as we want these expanded at runtime of init.sh
+cat <<EOF > /tmp/permissions_heredoc
+
+# get previous puid/pgid (if first run then will be empty string)
+previous_puid=\$(cat "/root/puid" 2>/dev/null || true)
+previous_pgid=\$(cat "/root/pgid" 2>/dev/null || true)
+
+# if first run (no puid or pgid files in /tmp) or the PUID or PGID env vars are different
+# from the previous run then re-apply chown with current PUID and PGID values.
+if [[ ! -f "/root/puid" || ! -f "/root/pgid" || "\${previous_puid}" != "\${PUID}" || "\${previous_pgid}" != "\${PGID}" ]]; then
+
+	# set permissions inside container - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
+	chown -R "\${PUID}":"\${PGID}" ${install_paths}
+
+fi
+
+# write out current PUID and PGID to files in /root (used to compare on next run)
+echo "\${PUID}" > /root/puid
+echo "\${PGID}" > /root/pgid
+
+EOF
+
+# replace permissions placeholder string with contents of file (here doc)
+sed -i '/# PERMISSIONS_PLACEHOLDER/{
+    s/# PERMISSIONS_PLACEHOLDER//g
+    r /tmp/permissions_heredoc
+}' /usr/local/bin/init.sh
+rm /tmp/permissions_heredoc
+
+# env vars
+####
+
+# cleanup
+cleanup.sh
---
+++ run/nobody/start.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/dumb-init /bin/bash
+
+# specify argument values
+config_path="/config"
+webdir="/usr/share/jellyfin/web"
+ffmpeg_file_path="/usr/lib/jellyfin-ffmpeg/ffmpeg"
+
+# create folders to store config, data, cache and logs
+mkdir -p /config/config /config/data /config/cache /config/logs
+
+# run jellyfin with arguments
+/usr/lib/jellyfin/bin/jellyfin \
+--datadir "${config_path}/data" \
+--cachedir "${config_path}/cache" \
+--configdir "${config_path}/config" \
+--logdir "${config_path}/logs" \
+--ffmpeg "${ffmpeg_file_path}" \
+--webdir "${webdir}"
